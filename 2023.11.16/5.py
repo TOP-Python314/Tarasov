2944843650324bd629842c2cc6ed720d85b0b25f
Kirill <exultion@mail.ru>
2023-11-20 13:25:16 +0300

    Выполнил ДЗ за 16.11.2023

---
+++ 2023.11.16/5.py
@@ -0,0 +1,12 @@
+input_1 = input()
+input_2 = input()
+
+num_mile = float(input_1 + '.' + input_2)
+num_km = round(num_mile * 1.61, 1)
+
+print(f'{num_mile} миль = {num_km} км')
+
+#Вывод
+#6
+#6
+#6.6 миль = 10.6 км
