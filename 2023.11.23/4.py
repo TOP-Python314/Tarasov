aa70ed2ef8617a22ea1b455911bd390f32cb347d
Kirill <exultion@mail.ru>
2023-11-30 18:28:24 +0300

    дз 2023.11.23

--- 2023.11.23/4.py
+++ 2023.11.23/4.py
@@ -4,6 +4,14 @@ input2 = input("Введите координаты: ")
 sum1 = ord(input1[0]) + int(input1[1])
 sum2 = ord(input2[0]) + int(input2[1])
 
+if ord(input1[0]) >= ord('h') or ord(input2[0]) >= ord('h'):
+    print('выходит за H')
+    
+   
+if ord(input1[1]) >= int(ord('9')) or ord(input1[1]) >= int(ord('9')):
+    print('больше 8')
+    
+    
 if sum1 % 2 == sum2 % 2:
     print("да")
 else:
