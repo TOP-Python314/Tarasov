49d4ff2391bd389e5eebb3ab5ab657e977489d8f
Kirill <exultion@mail.ru>
2023-12-02 16:42:46 +0300

    подправка кода

--- 2023.11.23/4.py
+++ 2023.11.23/4.py
@@ -1,15 +1,15 @@
-input1 = input("Введите координаты: ")
-input2 = input("Введите координаты: ")
+coordinates_1 = input("Введите координаты: ")
+coordinates_2 = input("Введите координаты: ")
 
-sum1 = ord(input1[0]) + int(input1[1])
-sum2 = ord(input2[0]) + int(input2[1])
+sum1 = ord(coordinates_1[0]) + int(coordinates_1[1])
+sum2 = ord(coordinates_2[0]) + int(coordinates_2[1])
 
-if ord(input1[0]) >= ord('h') or ord(input2[0]) >= ord('h'):
+if ord(coordinates_1[0]) >= ord('h') or ord(coordinates_2[0]) >= ord('h'):
     print('выходит за H')
     
    
-if ord(input1[1]) >= int(ord('9')) or ord(input1[1]) >= int(ord('9')):
-    print('больше 8')
+if ord(coordinates_1[1]) >= int(ord('9')) or ord(coordinates_1[1]) >= int(ord('9')):
+    print('выходит за 8')
     
     
 if sum1 % 2 == sum2 % 2:
