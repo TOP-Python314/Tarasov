50189bb00e9df49118ddcc1a749a16f5d3d1c1be
Kirill <exultion@mail.ru>
2023-12-02 16:42:35 +0300

    подправка кода

--- 2023.11.23/3.py
+++ 2023.11.23/3.py
@@ -1,6 +1,6 @@
-input = int(input("Введите год: "))
+year = int(input("Введите год: "))
 
-if input % 4 == 0 and input % 100 != 0 or input % 400 == 0:
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print("да")
 else:
     print("нет")
